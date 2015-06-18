8eae00451d602eeeccce3b3f80f886927b3ba254
ram-thekratos <ramnarayannagarajan@outlook.com>
2015-06-18 22:28:22 +0530

    Login page and adding node server

---
+++ app/controllers/loginCtrl.js
@@ -0,0 +1,33 @@
+var masterCardApp = angular.module('masterCardApp',['ui.bootstrap']);
+
+masterCardApp.controller('loginCtrl', ['$scope', '$modal',function($scope,$modal) {
+  $scope.login = function() { window.alert("testing login"); };
+
+  $scope.register = function(){
+
+  	var modalInstance = $modal.open({
+      templateUrl: 'registration.html',
+      controller: registrationCtrl,
+      scope: $scope
+    });
+
+    modalInstance.result.then(function () {
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+  	
+  }]);
+
+  masterCardApp.controller('registrationCtrl', ['$scope','$modalInstance', function($scope,$modalInstance) {
+
+  $scope.ok = function () {
+  	window.alert("registering user...");
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+
+}]);
